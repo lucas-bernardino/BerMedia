5cb92bd93e9df997f02e310ea7a68b20998100c2
lucas-bernardino <lucasgber@gmail.com>
2024-01-25 18:25:02 -0300

    Change scraping by using BeautifulSoup instead of Selenium

--- server/utils/scrape.py
+++ server/utils/scrape.py
@@ -1,108 +1,71 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from time import sleep
+import requests
+from bs4 import BeautifulSoup
 import json
 
-class Scrape:
+def getData(url, type):
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.text,"html.parser")
     
-    def __init__(self, url, type):
-        self.url = url
-        self.raw_data_dict = {}
-        self.list_data = []
-        self.type = type
+    raw_data = soup.find(id="__NEXT_DATA__")
+    STRING_TO_BE_REPLACED ='<script id="__NEXT_DATA__" type="application/json">'
+    data = str(raw_data).replace(STRING_TO_BE_REPLACED, '').replace('</script>', '')
+    data_json_raw = json.loads(data)
     
-    def getRawData(self):
-        options = Options()
-        # options.add_argument("--headless=new")
-        driver = webdriver.Chrome(options=options)
-        driver.get(self.url)
+    all_data = data_json_raw["props"]['pageProps']["pageData"]["chartTitles"]["edges"]
 
-        pos_props = driver.page_source.find('{"props"')
-        temp1 = driver.page_source[pos_props:]
+    list_data = []
+
+    for data in all_data:
+ 
+        dict_data = {}
         
-        if temp1[0] != "{":
-            pos_props = driver.page_source.find('{"props"')
-            temp1 = driver.page_source[pos_props:] 
-        
-        
-        pos_requestContext = temp1.find(',"requestContext"')
-        temp2 = temp1[pos_requestContext:]
-        
-        
-        data_str = temp1.replace(temp2, "")
-        data_str = data_str + "}}}"
-        
-        driver.quit()
-        data_dict = json.loads(data_str)
-        self.raw_data_dict = data_dict
-    
-    
-    def getCleanData(self):
-        all_data = self.raw_data_dict["props"]['pageProps']["pageData"]["chartTitles"]["edges"]
-        
-        for data in all_data:
-            dict_data = {}
+        try:
+            rank = data["currentRank"]
+            titulo = data["node"]["originalTitleText"]["text"]
+            url = data["node"]["primaryImage"]["url"]
+            genero = data["node"]["titleGenres"]["genres"][0]["genre"]["text"]
+            nota = data["node"]["ratingsSummary"]["aggregateRating"]
+            plot = data["node"]["plot"]["plotText"]["plainText"]
+            imdb_id = data["node"]["id"]
+
             
-            try:
-                rank = data["currentRank"]
-                titulo = data["node"]["titleText"]["text"]
-                url = data["node"]["primaryImage"]["url"]
-                genero = data["node"]["titleGenres"]["genres"][0]["genre"]["text"]
-                nota = data["node"]["ratingsSummary"]["aggregateRating"]
-                plot = data["node"]["plot"]["plotText"]["plainText"]
-                imdb_id = data["node"]["id"] 
+            if type == "movie":
+                ano = data["node"]["releaseYear"]["year"]
+                horas = data["node"]["runtime"]["seconds"] // 3600
+                minut = int(((data["node"]["runtime"]["seconds"] / 3600) % horas) * 60)
                 
-                if self.type == "movie":
-                    ano = data["node"]["releaseYear"]["year"]
-                    horas = data["node"]["runtime"]["seconds"] // 3600
-                    minut = int(((data["node"]["runtime"]["seconds"] / 3600) % horas) * 60)
-                    
-                    dict_data = {
-                        "id": imdb_id,
-                        "url": url,
-                        "title": titulo,
-                        "rank": rank,
-                        "plot": plot,
-                        "genre": genero,
-                        "rating": nota,
-                        "year": ano,
-                        "length": {"hour": horas, "min": minut}
-                    }        
+                dict_data = {
+                    "id": imdb_id,
+                    "url": url,
+                    "title": titulo,
+                    "rank": rank,
+                    "plot": plot,
+                    "genre": genero,
+                    "rating": nota,
+                    "year": ano,
+                    "length": {"hour": horas, "min": minut}
+                }
+            
+            if type == "show":
+                ano_inicio = data["node"]["releaseYear"]["year"]
+                ano_fim = data["node"]["releaseYear"]["endYear"] if data["node"]["releaseYear"]["endYear"] is not None else -1
                 
-                if self.type == "show":
-                    ano_inicio = data["node"]["releaseYear"]["year"]
-                    ano_fim = data["node"]["releaseYear"]["endYear"] if data["node"]["releaseYear"]["endYear"] is not None else -1
-                    
-                    dict_data = {
-                        "id": imdb_id,
-                        "url": url,
-                        "title": titulo,
-                        "rank": rank,
-                        "plot": plot,
-                        "genre": genero,
-                        "rating": nota,
-                        "year_start": ano_inicio,
-                        "year_end": ano_fim,
-                    }    
-                      
-                self.list_data.append(dict_data)
-            
-            except Exception as e:
-                pass
-            
-    
+                dict_data = {
+                    "id": imdb_id,
+                    "url": url,
+                    "title": titulo,
+                    "rank": rank,
+                    "plot": plot,
+                    "genre": genero,
+                    "rating": nota,
+                    "year_start": ano_inicio,
+                    "year_end": ano_fim,
+                }    
+                
+            list_data.append(dict_data)
         
-    
-    def getData(self):
-        self.getRawData()
-        self.getCleanData()
-        return self.list_data
+        except Exception as e:
+            pass
 
-
-if __name__ == "__main__":
-    testeTopMovie = Scrape("https://www.imdb.com/chart/top/", "movie")
-    print(testeTopMovie.getData())
+    return list_data
--- server/utils/server.py
+++ server/utils/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from scrape import Scrape
+from scrape import getData
 
 URL_TOP_MOVIE = "https://www.imdb.com/chart/top/"
 URL_POP_MOVIE = "https://www.imdb.com/chart/moviemeter/"
@@ -12,23 +12,23 @@ CORS(app)
 
 @app.route("/topmovies")
 def getTopMovies():
-    TopMovies = Scrape(URL_TOP_MOVIE, "movie")
-    return jsonify( TopMovies.getData() )
+    top_movies = getData(URL_TOP_MOVIE, "movie")
+    return jsonify( top_movies )
 
 @app.route("/popmovies")
 def getPopMovies():
-    PopMovies = Scrape(URL_POP_MOVIE, "movie")
-    return jsonify( PopMovies.getData() )
+    pop_movies = getData(URL_POP_MOVIE, "movie")
+    return jsonify( pop_movies )
 
 @app.route("/topshows")
 def getTopShows():
-    TopShows = Scrape(URL_TOP_SHOW, "show")
-    return jsonify( TopShows.getData() )
+    top_shows = getData(URL_TOP_SHOW, "show")
+    return jsonify( top_shows )
 
 @app.route("/popshows")
 def getPopShows():
-    PopShows = Scrape(URL_POP_SHOW, "show")
-    return jsonify( PopShows.getData() )
+    pop_shows = getData(URL_POP_SHOW, "show")
+    return jsonify( pop_shows )
 
 if __name__ == "__main__":
     app.run(debug=True)
