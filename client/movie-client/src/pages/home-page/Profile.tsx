8feae9fefa072e66a695dc55b761e557fab713f9
lucas-bernardino <lucasgber@gmail.com>
2024-02-08 10:28:06 -0300

    Start working on profile page

---
+++ client/movie-client/src/pages/home-page/Profile.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import {
+  getAuthenticatedUser,
+  getTokenLocalStorage,
+} from "../../utils/helpers";
+import { useNavigate } from "react-router-dom";
+import { IUser } from "../../utils/interfaces";
+import Card from "../../components/card/Card";
+import Navbar from "../../components/navbar/Navbar";
+
+function Profile() {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState<IUser | null>();
+  const [token, setToken] = useState<string | null>();
+
+  const isAuthenticated = async () => {
+    const userDetails = await getAuthenticatedUser();
+    if (!userDetails) {
+      navigate("/signin");
+      return;
+    }
+    setUser(userDetails);
+    setToken(getTokenLocalStorage());
+  };
+
+  useEffect(() => {
+    isAuthenticated();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(user?.medias);
+
+  return (
+    <>
+      <div className="sticky top-0 z-50 flex justify-center bg-gradient-to-r from-slate-900 via-slate-700 to-slate-900">
+        <Navbar />
+      </div>
+      <div>
+        {user ? (
+          <div className="relative bg-gradient-to-r from-slate-900 via-slate-700 to-slate-900 flex-wrap flex content-center justify-center min-h-screen">
+            {user.medias.map((media) => (
+              <Card media={media} />
+            ))}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Profile;
+
+// const user = await getAuthenticatedUser();
+// if (!user) {
+//   navigate("/signin");
+//   return;
+// }
