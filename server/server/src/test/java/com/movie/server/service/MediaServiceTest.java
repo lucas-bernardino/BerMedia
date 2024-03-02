17650c20f91b9ef3ce40f4357fe4d46376d437ad
lucas-bernardino <lucasgber@gmail.com>
2024-03-02 17:42:52 -0300

    Add tests for getAllMediasByUsername in MediaServiceTest

--- server/server/src/test/java/com/movie/server/service/MediaServiceTest.java
+++ server/server/src/test/java/com/movie/server/service/MediaServiceTest.java
@@ -17,6 +17,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -36,9 +37,11 @@ public class MediaServiceTest {
     @InjectMocks
     MediaService mediaService;
 
+
+
     @Test
     @DisplayName("Should create a media sucessfully")
-    void createMediaSucessfully() {
+    void createMediaSuccessfully() {
         User user = new User(1L, "lucas",  "senha", List.of(), Role.USER);
         Media media = new Media(1L,
                 "The Shawshank Redemption",
@@ -90,5 +93,66 @@ public class MediaServiceTest {
 
     }
 
+    @Test
+    @DisplayName("Should get medias by username sucessfully")
+    void getAllMediasByUsernameSuccessfully() {
+        User user = new User(1L, "lucas",  "senha", List.of(), Role.USER);
+        Media media1 = new Media(1L,
+                "The Shawshank Redemption",
+                "tt0111161",
+                "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
+                "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
+                "16",
+                List.of("Drama"),
+                8520,
+                9.3F,
+                1,
+                "movie",
+                1994,
+                0,
+                List.of(user),
+                List.of());
+
+        Media media2 = new Media(2L,
+                "Breaking Bad",
+                "tt0903747",
+                "A chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine with a former student in order to secure his family's future.",
+                "https://m.media-amazon.com/images/M/MV5BYmQ4YWMxYjUtNjZmYi00MDQ1LWFjMjMtNjA5ZDdiYjdiODU5XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg",
+                "16",
+                List.of("Crime", "Drama", "Thriller"),
+                2700,
+                9.5F,
+                1,
+                "tvSeries",
+                2008,
+                2013,
+                List.of(user),
+                List.of());
+
+        user.setMedias(List.of(media1, media2));
+
+        when(userService.getUserByUsername(user.getUsername())).thenReturn(Optional.of(user));
+
+        List<Media> medias = mediaService.getAllMediasByUsername(user.getUsername());
+
+        Assertions.assertAll(() -> {
+            Assertions.assertTrue(medias.contains(media1));
+            Assertions.assertTrue(medias.contains(media2));
+        });
+
+    }
+
+    @Test
+    @DisplayName("Should thrown exception when getting medias with null username")
+    void getAllMediasByUsernameException1() {
+        User user = new User(1L, null,  "senha", List.of(), Role.USER);
+
+        Exception thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            mediaService.getAllMediasByUsername(user.getUsername());
+        });
+
+        Assertions.assertEquals("Username argument must not be null", thrown.getMessage());
+
+    }
 
 }
